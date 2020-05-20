c324a5148931b9416885882bc72d162f3e0cf6d3
blumberk1990 <magda.hubert.magda@gmail.com>
2020-05-20 17:39:32 +0200

    Maven

---
+++ 01_MavenDependencies/src/main/java/pl/mgd/utils/NameVerifier.java
@@ -0,0 +1,12 @@
+package pl.mgd.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class NameVerifier {
+
+	public static void main(String[] args) {
+		String name = "Hubi45";
+		System.out.println(StringUtils.isAlpha(name));
+	}
+
+}
